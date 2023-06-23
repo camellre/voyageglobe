7fc4fa811d57d16c93524d12b33d95d60caa5153
Rory Luo <camellre@gmail.com>
2023-06-23 15:37:13 -0700

    add customized css reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "sanitize.css";
-import "sanitize.css/forms.css";
-import "sanitize.css/typography.css";
 import "./index.css";
+import App from "./App.tsx";
+// import "sanitize.css";
+// import "sanitize.css/forms.css";
+// import "sanitize.css/typography.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
