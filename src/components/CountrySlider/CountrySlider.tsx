3d26a3d9b8cf9954fa1e34797807f045151d7d26
Rory Luo <camellre@gmail.com>
2023-07-31 16:18:37 -0700

    update country slider

--- src/components/CountrySlider/CountrySlider.tsx
+++ src/components/CountrySlider/CountrySlider.tsx
@@ -20,67 +20,67 @@ import Button from "../../genericComponents/Button/Button";
 
 function CountrySlider() {
   const antiguaContent = {
-    title: "安提瓜与巴布达",
+    title: "Antigua and Barbuda",
     bodyText:
-      "作为加勒比海地区最受欢迎的第二公民身份项目之一，安提瓜和巴布达提供了许多优势和机会。该项目不仅能够帮助申请人获得安全、稳定的居住权，还可以享受包括签证便利、资产保护、税收优惠等在内的多种福利，是一个极具吸引力的选择。",
-    ad_1: "3个月",
-    ad_2: "4代入籍",
-    ad_3: "10万美元起",
+      "As one of the most popular second citizenship programs in the Caribbean region, Antigua and Barbuda offer many advantages and opportunities. This program not only helps applicants secure stable residency rights, but also provides a range of benefits including visa convenience, asset protection, and tax advantages, making it a highly attractive choice.",
+    ad_1: "3 Months",
+    ad_2: "4 Generations",
+    ad_3: "Starting With USD100,000",
     background: antiguaBackground,
     to: "/citizenship/antigua",
   };
 
   const dominicaContent = {
-    title: "多米尼加",
+    title: "Dominica",
     bodyText:
-      "获得多米尼加第二公民身份后,申请人可以获得该国护照，这是一个享有许多优势的护照，如可以免签或落地签证进入130多个国家和地区，包括欧洲、亚洲和南美洲等。此外，多米尼加的护照审批速度较快，通常只需要3个月即可获得。多米尼加的投资入籍方案相对于其他国家的方案来说，投资金额比较低，程序较为简单和迅速，这使得该项目吸引了许多投资者和家庭的关注。",
-    ad_1: "3个月",
-    ad_2: "130+免签国",
-    ad_3: "10万美元起",
+      "After obtaining Dominican second citizenship, applicants can get a passport from the country, which has many advantages, such as visa-free or visa-on-arrival entry into more than 130 countries and regions, including Europe, Asia, and South America. Additionally, the approval speed for Dominican passports is relatively fast, usually requiring only 3 months. Compared to programs from other countries, the Dominican investment citizenship scheme requires a relatively low investment amount and has a simpler and faster procedure, attracting the attention of many investors and families.",
+    ad_1: "3 Months",
+    ad_2: "130+ Visa-Free Countries",
+    ad_3: "Starting With USD100,000",
     background: dominicaBackground,
     to: "/citizenship/dominica",
   };
 
   const grenadaContent = {
-    title: "格林纳达",
+    title: "Grenada",
     bodyText:
-      "格林纳达是一个位于加勒比海东南部的岛国，首都为圣乔治。拥有美丽的海滩和热带雨林风光，每年约吸引19万游客前往度假。格林纳达以高度重视公民福利而著名，其教育、医疗和税务政策惠民为国民提供了全面保障。林纳达护照在全球范围内具有较高的通行能力，可免签140多国家，全球开户。格林纳达是英联邦成员国之一，拥有稳定的民主政治体系，税务。格林纳达的投资入籍方案审批速度较快，通常只需要3-4个月即可获得格林纳达国籍和护照，这使得该项目成为许多人寻求第二公民身份的首选之一。",
-    ad_1: "4个月",
-    ad_2: "130+免签国",
-    ad_3: "15万美元起",
+      "Grenada is an island nation located in the southeast of the Caribbean Sea, with St. George as its capital. With beautiful beaches and tropical rainforest scenery, it attracts about 190,000 tourists annually for vacation. Grenada is famous for its high regard for citizen welfare, providing comprehensive protection for its citizens through favorable education, medical and tax policies. A Grenadian passport has a high level of global mobility, allowing visa-free access to more than 140 countries, and global account opening. Grenada is one of the Commonwealth member countries, with a stable democratic political system and taxation. The approval speed of Grenada's investment citizenship program is relatively fast, usually only requiring 3-4 months to obtain Grenadian nationality and passport, making this program one of the top choices for many people seeking second citizenship.",
+    ad_1: "4 Months",
+    ad_2: "130+ Visa-Free Countries",
+    ad_3: "Starting With USD150,000",
     background: grenadaBackground,
     to: "/citizenship/grenada",
   };
 
   const maltaContent = {
-    title: "马耳他",
+    title: "Malta",
     bodyText:
-      "马耳他的投资入籍项目是全球最知名的之一，该项目要求申请人进行一定的投资才能获得马耳他国籍。根据规定，申请人需要通过购买不动产、国债、股权等方式投资至少95万欧元，并在获得居留权后保持投资至少1年。通常情况下，整个投资入籍程序需要12-14个月左右的时间，其中包括居留权审批、投资安排等环节。投资入籍程序期间需要支付一些相关费用。总体而言，马耳他的投资入籍项目要求投资金额较高，程序时间较长，但可以获得欧盟护照和高度的签证自由度，因此备受投资者的青睐。",
-    ad_1: "1年",
-    ad_2: "186免签国",
-    ad_3: "95万欧元起",
+      "Malta's investment citizenship program is one of the most well-known globally, requiring applicants to make certain investments to obtain Maltese nationality. According to the regulations, applicants need to invest at least 950,000 Euros through purchasing real estate, government bonds, shares, etc., and maintain the investment for at least one year after obtaining residency. Usually, the entire investment citizenship process takes about 12-14 months, including residency approval, investment arrangement stages, and some related fees need to be paid during the process. In general, Malta's investment citizenship program requires a high investment amount and has a longer process, but it offers an EU passport and a high degree of visa freedom, hence it's favored by investors.",
+    ad_1: "12 Months",
+    ad_2: "186 Visa-Free Countries",
+    ad_3: "Starting With 950,000 Euros",
     background: maltaBackground,
     to: "/citizenship/malta",
   };
 
   const portugalContent = {
-    title: "葡萄牙",
+    title: "St. Lucia",
     bodyText:
-      "葡萄牙是一个美丽的欧洲国家，其第二公民身份项目是欧洲最有吸引力的之一。葡萄牙的第二公民身份项目可以为申请人提供在欧洲工作和生活的机会，以及享受许多社会福利和公共服务，如医疗保健和教育等。此外，葡萄牙还拥有优美的自然环境和丰富的文化历史，是一个理想的旅游胜地和生活地点。通过获得葡萄牙的第二公民身份，申请人可以享受到这些福利和优势，为其未来的发展和生活带来更多可能性。",
-    ad_1: "3年",
-    ad_2: "195免签国",
-    ad_3: "35万欧元起",
+      "St. Lucia lies in the eastern Caribbean Sea, northwest of Barbados and south of Martinique. A volcanic island mostly covered in rainforest, it is famous for its twin peaks — the Pitons — and its magical beaches. St. Lucia is a member of the Commonwealth and CARICOM and has excellent air links to Europe and North America.",
+    ad_1: "3-4 Months",
+    ad_2: "146 Visa-Free Countries",
+    ad_3: "Starting With $100,000",
     background: portugalBackground,
     to: "/citizenship/portugal",
   };
 
   const stkittisContent = {
-    title: "圣基茨",
+    title: "St. Kitts and Nevis",
     bodyText:
-      "圣基茨和尼维斯是一个加勒比海岛国，其第二公民身份项目是全球最受欢迎的之一。申请人获得第二公民身份后，将获得多种福利，包括免签证旅行、资产保护、税收优惠等，这些福利可以帮助申请人在国际商务和投资中获得更多机会和优势。此外，圣基茨的第二公民身份项目审批速度较快，通常只需要2-3个月即可获得，这使其成为许多人寻求第二公民身份的首选。因此，圣基茨的第二公民身份项目具有多种优势，为申请人带来了很多机会和便利。",
-    ad_1: "8个月",
-    ad_2: "139+免签国",
-    ad_3: "30万美元起",
+      "St. Kitts and Nevis is a Caribbean island nation, and its second citizenship program is one of the most popular globally. Once applicants obtain second citizenship, they receive numerous benefits, including visa-free travel, asset protection, tax advantages, etc. These benefits can provide applicants with more opportunities and advantages in international business and investments. Moreover, the approval speed of St. Kitts' second citizenship program is relatively fast, usually only taking 2-3 months. This makes it a top choice for many seeking second citizenship. Therefore, St. Kitts' second citizenship program offers a variety of advantages, bringing many opportunities and conveniences to applicants.",
+    ad_1: "4 Months",
+    ad_2: "139+ Visa-Free Countries",
+    ad_3: "Starting With $250,000",
     background: stkittsBackground,
     to: "/citizenship/stkitts",
   };
