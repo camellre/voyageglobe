130a437241a79c63bed8698f59a71daa64974c94
Rory Luo <camellre@gmail.com>
2023-08-02 16:37:11 -0700

    update country slider

--- src/components/CountrySlider/CountrySlider.tsx
+++ src/components/CountrySlider/CountrySlider.tsx
@@ -33,7 +33,7 @@ function CountrySlider() {
   const dominicaContent = {
     title: "Dominica",
     bodyText:
-      "After obtaining Dominican second citizenship, applicants can get a passport from the country, which has many advantages, such as visa-free or visa-on-arrival entry into more than 130 countries and regions, including Europe, Asia, and South America. Additionally, the approval speed for Dominican passports is relatively fast, usually requiring only 3 months. Compared to programs from other countries, the Dominican investment citizenship scheme requires a relatively low investment amount and has a simpler and faster procedure, attracting the attention of many investors and families.",
+      "Dominican Passport provides visa-free or visa-on-arrival entry into more than 130 countries and regions. Additionally, the approval speed for Dominican passports is relatively fast, usually requiring only 3 months. Compared to programs from other countries, the Dominican citizenship by investment requires a relatively low investment amount and has a simpler and faster procedure.",
     ad_1: "3 Months",
     ad_2: "130+ Visa-Free Countries",
     ad_3: "Starting With USD100,000",
@@ -44,7 +44,7 @@ function CountrySlider() {
   const grenadaContent = {
     title: "Grenada",
     bodyText:
-      "Grenada is an island nation located in the southeast of the Caribbean Sea, with St. George as its capital. With beautiful beaches and tropical rainforest scenery, it attracts about 190,000 tourists annually for vacation. Grenada is famous for its high regard for citizen welfare, providing comprehensive protection for its citizens through favorable education, medical and tax policies. A Grenadian passport has a high level of global mobility, allowing visa-free access to more than 140 countries, and global account opening. Grenada is one of the Commonwealth member countries, with a stable democratic political system and taxation. The approval speed of Grenada's investment citizenship program is relatively fast, usually only requiring 3-4 months to obtain Grenadian nationality and passport, making this program one of the top choices for many people seeking second citizenship.",
+      "A Grenadian passport has a high level of global mobility, allowing visa-free access to more than 140 countries, and global account opening. Grenada is one of the Commonwealth member countries, with a stable democratic political system and taxation. The approval speed of Grenada's investment citizenship program is relatively fast, usually only requiring 3-4 months to obtain Grenadian nationality and passport.",
     ad_1: "4 Months",
     ad_2: "130+ Visa-Free Countries",
     ad_3: "Starting With USD150,000",
@@ -55,7 +55,7 @@ function CountrySlider() {
   const maltaContent = {
     title: "Malta",
     bodyText:
-      "Malta's investment citizenship program is one of the most well-known globally, requiring applicants to make certain investments to obtain Maltese nationality. According to the regulations, applicants need to invest at least 950,000 Euros through purchasing real estate, government bonds, shares, etc., and maintain the investment for at least one year after obtaining residency. Usually, the entire investment citizenship process takes about 12-14 months, including residency approval, investment arrangement stages, and some related fees need to be paid during the process. In general, Malta's investment citizenship program requires a high investment amount and has a longer process, but it offers an EU passport and a high degree of visa freedom, hence it's favored by investors.",
+      "Malta's investment citizenship program is one of the most well-known globally, requiring applicants to make certain investments to obtain Maltese nationality. In general, Malta's investment citizenship program requires a high investment amount and has a longer process, but it offers an EU passport and a high degree of visa freedom, hence it's favored by investors.",
     ad_1: "12 Months",
     ad_2: "186 Visa-Free Countries",
     ad_3: "Starting With 950,000 Euros",
@@ -77,7 +77,7 @@ function CountrySlider() {
   const stkittisContent = {
     title: "St. Kitts and Nevis",
     bodyText:
-      "St. Kitts and Nevis is a Caribbean island nation, and its second citizenship program is one of the most popular globally. Once applicants obtain second citizenship, they receive numerous benefits, including visa-free travel, asset protection, tax advantages, etc. These benefits can provide applicants with more opportunities and advantages in international business and investments. Moreover, the approval speed of St. Kitts' second citizenship program is relatively fast, usually only taking 2-3 months. This makes it a top choice for many seeking second citizenship. Therefore, St. Kitts' second citizenship program offers a variety of advantages, bringing many opportunities and conveniences to applicants.",
+      "St. Kitts and Nevis Passport provides visa-free travel, asset protection, tax advantages, etc. These benefits can provide applicants with more opportunities and advantages in international business and investments. Moreover, the approval speed of St. Kitts' second citizenship program is relatively fast, usually only taking 2-3 months. ",
     ad_1: "4 Months",
     ad_2: "139+ Visa-Free Countries",
     ad_3: "Starting With $250,000",
@@ -88,7 +88,7 @@ function CountrySlider() {
   const turkeyContent = {
     title: "Turkey",
     bodyText:
-      "Turkey is a country uniquely located on the Eurasian continent, and its second citizenship program also has many advantages. As a country with diverse culture, advanced economy, and strategic location, Turkey's second citizenship can bring applicants numerous benefits, including visa-free travel, asset protection, and tax advantages. Additionally, Turkey's second citizenship can bring more opportunities and advantages for applicants in international business and investment, bringing more possibilities for their future career development.",
+      "Turkey is a country uniquely located on the Eurasian continent, and its second citizenship program also has many advantages. As a country with diverse culture, advanced economy, and strategic location, Turkey's second citizenship can bring applicants numerous benefits, including visa-free travel, asset protection, and tax advantages. ",
     ad_1: "8 Months",
     ad_2: "US E-2 Visa",
     ad_3: "Starting With $300,000",
@@ -99,7 +99,7 @@ function CountrySlider() {
   const usaContent = {
     title: "United States",
     bodyText:
-      "The United States, a global superpower, attracts countless high-net-worth individuals with its comprehensive advantages and passport strength. As a U.S. citizen, you will enjoy one of the world's most developed welfare systems, with top-notch medical, pension, and social security. High-quality family life, superior real estate, tax, and investment environment make your family life more stable. Additionally, U.S. educational resources are world-leading, with top universities such as Harvard and Yale, providing broad development space for children. In summary, becoming a U.S. citizen means gaining a better quality of life, superior educational resources, and a globally recognized passport, an ideal choice for high-net-worth individuals.",
+      "The United States, a global superpower, attracts countless high-net-worth individuals with its comprehensive advantages and passport strength. High-quality family life, superior real estate, tax, and investment environment make your family life more stable. Additionally, U.S. educational resources are world-leading, with top universities such as Harvard and Yale, providing broad development space for children.",
     ad_1: "3 - 5 years",
     ad_2: "180+ Visa-Free Countries",
     ad_3: "Starting With $800,000",
@@ -146,7 +146,7 @@ function CountrySlider() {
               <p className={style.ad_2}>{item.ad_2}</p>
               <p className={style.ad_3}>{item.ad_3}</p>
             </div>
-            <Button text="Learn More" containerStyle={style.contentButton} />
+            <Button text="Learn More" containerStyle={style.button} />
           </div>
         </div>
       ))}
